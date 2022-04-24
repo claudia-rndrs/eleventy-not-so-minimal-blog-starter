a7cfc2682dbaf24d25518edde924167755f328a5
Claudia Reynders <>
2022-04-24 16:40:02 +0200

    Added eleventyNavigation plugin to config

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation')
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyNavigationPlugin)
+
   eleventyConfig.addLayoutAlias('page', 'layouts/page')
 
   return {
