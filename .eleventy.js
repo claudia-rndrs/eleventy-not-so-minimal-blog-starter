f38b722b0984d42897bd5dce89cd232ebf9d2f8e
Claudia Reynders <>
2022-04-25 20:47:32 +0200

    Created blog collection by filtering based on folder and markdown filetype

--- .eleventy.js
+++ .eleventy.js
@@ -5,10 +5,17 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addLayoutAlias('page', 'layouts/page')
 
+  /* Creating a collection of blogposts by filtering based on folder and filetype */
+  eleventyConfig.addCollection("blog", (collectionApi) => {
+    return collectionApi.getFilteredByGlob('./src/blog/*.md').reverse()
+  })
+
   return {
     dir: {
       input: 'src',
       output: '_site',
+      includes: '_includes',
+      data: '_data'
     },
     markdownTemplateEngine: 'njk'
   }
