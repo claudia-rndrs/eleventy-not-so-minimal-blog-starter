d99e9ccc97a92a1caeca06e06b69979c84661398
Kiri Vadivelu <kiri.vadivelu@gmail.com>
2023-04-07 13:31:41 -0400

    spelling error - categorised to categorized

--- .eleventy.js
+++ .eleventy.js
@@ -1,81 +1,70 @@
-const eleventyNavigationPlugin = require('@11ty/eleventy-navigation')
-const pluginRss = require('@11ty/eleventy-plugin-rss')
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
-const { 
-  getAllPosts, 
+const {
+  getAllPosts,
   getCategoryList,
-  getCategorisedPosts 
-} = require('./config/collections')
+  getCategorizedPosts,
+} = require("./config/collections");
 
-const { 
-  readableDate 
-} = require('./config/filters')
+const { readableDate } = require("./config/filters");
 
-const { 
-  imageShortcode 
-} = require('./config/shortcodes')
+const { imageShortcode } = require("./config/shortcodes");
 
-
-module.exports = function(eleventyConfig) {
-  
+module.exports = function (eleventyConfig) {
   /*================================*/
   /*   plugins and configurations   */
   /*================================*/
-  eleventyConfig.addPlugin(eleventyNavigationPlugin)
-  eleventyConfig.addPlugin(pluginRss)
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(pluginRss);
 
   eleventyConfig.setFrontMatterParsingOptions({
     excerpt: true,
     excerpt_separator: "<!-- excerpt -->",
-    excerpt_alias: 'excerpt'
-  })
+    excerpt_alias: "excerpt",
+  });
 
   /*===================================================*/
   /* files that need to be copied to the build folder  */
   /*===================================================*/
-  eleventyConfig.addPassthroughCopy('./src/assets/social-image.jpg')
-  eleventyConfig.addPassthroughCopy('./src/assets/icons')
-  eleventyConfig.addPassthroughCopy('./src/assets/sprite.svg')
+  eleventyConfig.addPassthroughCopy("./src/assets/social-image.jpg");
+  eleventyConfig.addPassthroughCopy("./src/assets/icons");
+  eleventyConfig.addPassthroughCopy("./src/assets/sprite.svg");
   eleventyConfig.addPassthroughCopy({
-      'node_modules/svg-icon-sprite/dist/svg-icon-sprite.js': 'assets/svg-icon-sprite.js'
-  })
-
+    "node_modules/svg-icon-sprite/dist/svg-icon-sprite.js":
+      "assets/svg-icon-sprite.js",
+  });
 
   /*=================*/
   /*     Layouts     */
   /*=================*/
-  eleventyConfig.addLayoutAlias('page', 'layouts/page')
-  eleventyConfig.addLayoutAlias('article', 'layouts/article')
-
+  eleventyConfig.addLayoutAlias("page", "layouts/page");
+  eleventyConfig.addLayoutAlias("article", "layouts/article");
 
   /*=================*/
   /*   Collections   */
   /*=================*/
-  eleventyConfig.addCollection('blog', getAllPosts)
-  eleventyConfig.addCollection('categoryList', getCategoryList)
-  eleventyConfig.addCollection('categorisedPosts', getCategorisedPosts)
+  eleventyConfig.addCollection("blog", getAllPosts);
+  eleventyConfig.addCollection("categoryList", getCategoryList);
+  eleventyConfig.addCollection("categorizedPosts", getCategorizedPosts);
 
-  
   /*=================*/
   /*     Filters     */
   /*=================*/
-  eleventyConfig.addFilter('readableDate', readableDate)
-
+  eleventyConfig.addFilter("readableDate", readableDate);
 
   /*=================*/
   /*    shortcodes   */
   /*=================*/
-  eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode)
-
-
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
 
   return {
     dir: {
-      input: 'src',
-      output: '_site',
-      includes: '_includes',
-      data: '_data'
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      data: "_data",
     },
-    markdownTemplateEngine: 'njk'
-  }
-}
+    markdownTemplateEngine: "njk",
+  };
+};
--- config/collections.js
+++ config/collections.js
@@ -1,77 +1,73 @@
-const slugify = require('slugify')
+const slugify = require("slugify");
 
 /* Creating a collection containing all blogposts by filtering based on folder and filetype */
 const getAllPosts = (collectionApi) => {
-  return collectionApi.getFilteredByGlob('./src/blog/*.md')
-  .reverse()
-}
+  return collectionApi.getFilteredByGlob("./src/blog/*.md").reverse();
+};
 
 const getCategoryList = (collectionApi) => {
-  const catPages = []
-  let categories = []
-  const blogPosts = collectionApi.getFilteredByGlob('./src/blog/*.md')
+  const catPages = [];
+  let categories = [];
+  const blogPosts = collectionApi.getFilteredByGlob("./src/blog/*.md");
 
   blogPosts.map((item) => {
-    categories.push(item.data.category)
-  })
+    categories.push(item.data.category);
+  });
 
-  categories = categories.sort(sortAlphabetically)
-  const temp = [...new Set(categories)]
+  categories = categories.sort(sortAlphabetically);
+  const temp = [...new Set(categories)];
 
   temp.forEach((category) => {
     const slug = strToSlug(category);
 
-    if(slug !== 'in-the-spotlight') {
+    if (slug !== "in-the-spotlight") {
       catPages.push({
-        'key': slug,
-        'name': category 
-      })
+        key: slug,
+        name: category,
+      });
     }
-  })
+  });
 
-  return catPages
-}
+  return catPages;
+};
 
-const getCategorisedPosts = (collectionApi) => {
-  const categorisedPosts = {}
+const getCategorizedPosts = (collectionApi) => {
+  const categorizedPosts = {};
 
-  collectionApi.getFilteredByGlob('./src/blog/*.md').forEach(item => {
-    const category = item.data.category
-      
+  collectionApi.getFilteredByGlob("./src/blog/*.md").forEach((item) => {
+    const category = item.data.category;
+
     // Ignore the ones without a category
-    if (typeof category !== 'string')
-    return
+    if (typeof category !== "string") return;
 
-    const slug = strToSlug(category)
+    const slug = strToSlug(category);
 
-    if (Array.isArray(categorisedPosts[slug])) {
-      categorisedPosts[slug].push(item)
+    if (Array.isArray(categorizedPosts[slug])) {
+      categorizedPosts[slug].push(item);
     } else {
-      categorisedPosts[slug] = [item]
+      categorizedPosts[slug] = [item];
     }
-  })
+  });
 
-  return categorisedPosts
-}
+  return categorizedPosts;
+};
 
 module.exports = {
   getAllPosts,
   getCategoryList,
-  getCategorisedPosts
-}
-
+  getCategorizedPosts,
+};
 
 function strToSlug(str) {
   const options = {
-      replacement: "-",
-      remove: /[&,+()$~%.'":*?<>{}]/g,
-      lower: true,
-  }
-  
-  return slugify(str, options)
+    replacement: "-",
+    remove: /[&,+()$~%.'":*?<>{}]/g,
+    lower: true,
+  };
+
+  return slugify(str, options);
 }
 
-
 function sortAlphabetically(a, b) {
-  return a.localeCompare(b, "en", { sensitivity: "base" })
+  return a.localeCompare(b, "en", { sensitivity: "base" });
 }
